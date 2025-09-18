5dc41fca537d030ca9bae424943e45e2af2578e5
Fredrik Larida <fredrik.larida@gmail.com>
2025-09-18 14:18:23 -0600

    Added a newInstance method for editing purposes

--- code/app/src/main/java/com/example/listycitylab3/AddCityFragment.java
+++ code/app/src/main/java/com/example/listycitylab3/AddCityFragment.java
@@ -19,6 +19,20 @@ public class AddCityFragment extends DialogFragment {
 
     private AddCityDialogListener listener;
 
+    public static AddCityFragment newInstance(String city, String province) {
+//        Idea taken from https://stackoverflow.com/questions/36113134/understanding-the-fragment-newinstance-method
+//        Authored by: Arkadiusz Konior
+//        Taken by: Fredrik Larida
+//        Taken on: 2025-09-18
+        AddCityFragment newFragment = new AddCityFragment();
+        Bundle args = new Bundle();
+        args.putString("city", city);
+        args.putString("province", province);
+        newFragment.setArguments(args);
+
+        return newFragment;
+    }
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -36,6 +50,8 @@ public class AddCityFragment extends DialogFragment {
                 LayoutInflater.from(getContext()).inflate(R.layout.fragment_add_city, null);
         EditText editCityName = view.findViewById(R.id.edit_text_city_text);
         EditText editProvinceName = view.findViewById(R.id.edit_text_province_text);
+
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         return builder
                 .setView(view)
